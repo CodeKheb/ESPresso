aae2741687c8aab290ca295f78739331f91e443b
CodeKheb <kherbinbuenaventura@gmail.com>
2026-06-12 17:29:27 +0800

    Brewing... Android Studio

---
+++ app/ESPresso/src-tauri/gen/android/app/src/main/java/com/CodeKheb/espresso/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.CodeKheb.espresso
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
