8bd5023c1c694621d11dc60f4bbe26f0f3edabed
CodeKheb <kherbinbuenaventura@gmail.com>
2026-06-19 16:30:20 +0800

    Removed Android Home Buttons

--- app/ESPresso/src-tauri/gen/android/app/src/main/java/com/CodeKheb/espresso/MainActivity.kt
+++ app/ESPresso/src-tauri/gen/android/app/src/main/java/com/CodeKheb/espresso/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.CodeKheb.espresso
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+    hideSystemBars()
+  }
+
+  override fun onWindowFocusChanged(hasFocus: Boolean) {
+    super.onWindowFocusChanged(hasFocus)
+    if (hasFocus) {
+      hideSystemBars()
+    }
+  }
+
+  private fun hideSystemBars() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      window.insetsController?.let {
+        it.hide(WindowInsets.Type.navigationBars())
+        it.systemBarsBehavior =
+          WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+      }
+    } else {
+      @Suppress("DEPRECATION")
+      window.decorView.systemUiVisibility = (
+        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        or View.SYSTEM_UI_FLAG_FULLSCREEN
+      )
+    }
   }
 }
